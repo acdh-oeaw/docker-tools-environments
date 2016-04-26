745f92556b74c0fb490201e78236a014c3d9d4a5
zozlak <zozlak@zozlak.org>
2016-04-26 15:31:57 +0200

    All RHEL (Red Hat Enterprise Linux) images were moved to the separate rhel_envs branch as all images we use are derived either from ubuntu ones or directly from centos one but no one inherits from rhel ones

--- rhel_existdb30/wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ ! "$(ls -A /opt/exist/autodeploy)" ];
-then cp /opt/exist/autodeploy.init/* /opt/exist/autodeploy
-fi
-/opt/exist/tools/wrapper/bin/wrapper-linux-x86-64 "$@"
--- rhel_java8/MinimalHTTPexample/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-javac com/stackoverflow/q3732109/MinimalHTTP.java
-jar -cmvf MANIFEST.MF MinimalHTTP.jar com/stackoverflow/q3732109/*.class
-java -jar MinimalHTTP.jar
