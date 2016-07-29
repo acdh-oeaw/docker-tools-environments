597e0992a2ff032c41074a7860f3af7d00e59d4f
mzoltak <mzoltak@hephaistos.arz.oeaw.ac.at>
2016-07-29 13:41:23 +0200

    Proprietary images moved to ssh://git@acdh.oeaw.ac.at/docker-restricted-images.git

--- noske:2.107.1/run_lighttpd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ ! "$(ls -A /usr/lib/python2.7/dist-packages/bonito)" ];
-then cp -R /usr/lib/python2.7/dist-packages/bonito.init/* /usr/lib/python2.7/dist-packages/bonito
-fi
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
