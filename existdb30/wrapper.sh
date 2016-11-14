c2ab86e80ee3057c0c3bc4e4079de996b5fae452
Dalibor Pancic <dalibor.pancic@oeaw.ac.at>
2016-11-14 10:28:39 +0100

    Updated SHA-384 value for Composer in corpus_shell environment.

--- existdb30/wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /opt/exist/autodeploy
-if [ ! "$(ls -A /opt/exist/autodeploy)" ];
-then cp /opt/exist/autodeploy.init/* /opt/exist/autodeploy
-fi
-/opt/exist/tools/wrapper/bin/wrapper-linux-x86-64 "$@"
--- noske/README.md
+++
@@ -1,2 +0,0 @@
-#4910
-Build with SELinux enforce fails because write access to /etc/group in the container is denied
