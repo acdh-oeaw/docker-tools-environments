6c230d851bca68e5cd56d9f68d3cf84e7c4cf6c2
root <root@eos.arz.oeaw.ac.at>
2016-11-22 14:48:19 +0100

    Drupal8 extended with composer run on environment run

---
+++ http_drupal8/updateLibs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+REQ=""
+for i in `find /var/www/html -name composer.json | grep -v /var/www/html/core | grep -v /var/www/html/composer.json | grep -v /var/www/html/vendor | sed -e 's|^/var/www/html/||'`; do
+  REQ=", \"$i\""
+done
+REQ="\"merge-plugin\": {\"require\":[${REQ:2}],"
+sed -i -e "s|^.*\"merge-plugin\".*$|$REQ|" /var/www/html/composer.json
+cd /var/www/html
+composer install
+composer update
