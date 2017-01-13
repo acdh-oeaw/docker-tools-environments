54e3b84a46818ceeecd0012babc0e3d3f997111a
Omar SIam <Omar.Siam@oeaw.ac.at>
2017-01-13 13:55:15 +0000

    Use EXIST_HOME for determining the path for log files. This stops long and verbose error messages about this on every command line tool. Remove some duplicate copy command. Docker communicates signals (like stop = SIGTERM) to PID 1 in the container. So make the wrapper PID 1. Should lead to a cleaner shutdown.

--- existdb30/wrapper.sh
+++ existdb30/wrapper.sh
@@ -3,4 +3,4 @@ mkdir -p /opt/exist/autodeploy
 if [ ! "$(ls -A /opt/exist/autodeploy)" ];
 then cp /opt/exist/autodeploy.init/* /opt/exist/autodeploy
 fi
-/opt/exist/tools/wrapper/bin/wrapper-linux-x86-64 "$@"
+exec /opt/exist/tools/wrapper/bin/wrapper-linux-x86-64 "$@"
