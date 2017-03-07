48da12c9fdada695a97efa0a596de10fd289a9cd
root <root@hephaistos.arz.oeaw.ac.at>
2017-03-07 11:38:39 +0100

    fixed problem with only (the last) one dependent composer.json being handled

--- http_drupal8/updateLibs.sh
+++ http_drupal8/updateLibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REQ=""
 for i in `find /var/www/html -name composer.json | grep -v /var/www/html/core | grep -v /var/www/html/composer.json | grep -v /var/www/html/vendor | sed -e 's|^/var/www/html/||'`; do
-  REQ=", \"$i\""
+  REQ="${REQ}, \"$i\""
 done
 REQ="\"merge-plugin\": {\"require\":[${REQ:2}],"
 sed -i -e "s|^.*\"merge-plugin\".*$|$REQ|" /var/www/html/composer.json
