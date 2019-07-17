4042e9c4c57371f846959687688950e01f325a41
Omar Siam <Omar.Siam@oeaw.ac.at>
2019-07-17 18:37:25 +0200

    Needs a custom build dir and the option to influence the build dir further.

--- basex_gitlab_runner/register-gitlab-runner.sh
+++ basex_gitlab_runner/register-gitlab-runner.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 gitlab-runner register \
   --config /etc/gitlab-runner/config.toml \
+  --builds-dir /opt/basex/webapp --custom_build_dir-enabled \
   --url "https://gitlab.com/" \
   --executor "shell" \
   --description "basex-environment" \
