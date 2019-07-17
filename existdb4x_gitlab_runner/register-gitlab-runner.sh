2697d15bfed450d5e8d4d12bf048c0332ff1a02c
Omar Siam <omar.siam@oeaw.ac.at>
2019-07-17 17:40:43 +0200

    Need to set the "correct" location of gitlab-runner config file.

--- existdb4x_gitlab_runner/register-gitlab-runner.sh
+++ existdb4x_gitlab_runner/register-gitlab-runner.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 gitlab-runner register \
+  --config /etc/gitlab-runner/config.toml \
   --url "https://gitlab.com/" \
   --executor "shell" \
   --description "existdb4x-environment" \
