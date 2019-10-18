71994d60cb703c2d55d0c692e6f8c2d728b2210f
Omar Siam <omar.siam@oeaw.ac.at>
2019-10-18 13:40:30 +0000

    docker-tools needs some shell programs to work at all. Add them.

---
+++ existdb5x/groupadd.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Translates arguments between standard groupadd and busybox'es addgroup
+# busybox does not accept long args
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -g|--gid)
+    TR_GID="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+addgroup -g $TR_GID $@
---
+++ existdb5x/useradd.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Translates arguments between standard useradd and busybox'es adduser
+# busybox does not accept long args, uses -h instead of -d
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -u|--uid)
+    TR_UID="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -g|--gid)
+    TR_GID="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--home-dir)
+    TR_HOMEDIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+adduser -S -u $TR_UID -G $@ -h $TR_HOMEDIR $@
