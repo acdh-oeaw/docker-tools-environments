87be35e63a6be30b4b57b54599b5c503d272b4a1
Omar Siam <omar.siam@oeaw.ac.at>
2019-10-18 13:12:58 +0000

    Base 5x exist db environments on the official docker container.

--- existdb5x/wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /opt/exist/autodeploy
-if [ ! "$(ls -A /opt/exist/autodeploy)" ];
-then cp /opt/exist/autodeploy.init/* /opt/exist/autodeploy
-fi
-exec ${JAVA_HOME}/bin/java -Dwrapper.pidfile=${EXIST_HOME}/webapp/WEB-INF/logs/wrapper.pid -Dwrapper.service=true -Dwrapper.visible=false -Djna_tmpdir=${EXIST_HOME}/tools/yajsw/tmp -jar ${EXIST_HOME}/tools/yajsw/wrapper.jar -c ${EXIST_HOME}/tools/yajsw/conf/wrapper.conf
