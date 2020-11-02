824e9c2f660e45dd9431e030cb74b879e01be0df
Omar Siam <Omar.Siam@oeaw.ac.at>
2020-11-02 18:45:58 +0000

    Copy crystal as packaged to a working directory on first run. Change the log file format to some JSON style that could be processed by matomo and add an env var to set the site id.

--- noske/run_lighttpd.sh
+++ noske/run_lighttpd.sh
@@ -2,5 +2,8 @@
 if [ ! "$(ls -A /usr/lib/python2.7/dist-packages/bonito)" ];
 then cp -R /usr/lib/python2.7/site-packages/bonito.init/* /usr/lib/python2.7/dist-packages/bonito
 fi
+if [ ! "$(ls -A /var/www/crystal)" ];
+then cp -R /var/www/crystal.init/* /var/www/crystal
+fi
 exec 3>&1
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
