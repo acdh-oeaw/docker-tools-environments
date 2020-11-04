6be919f466e4039a0c5e773676dc5a1391fbd5a8
Omar Siam <Omar.Siam@oeaw.ac.at>
2020-11-04 12:19:23 +0100

    Backport changes from noske crystal variant.

--- noske/run_lighttpd.sh
+++ noske/run_lighttpd.sh
@@ -2,4 +2,5 @@
 if [ ! "$(ls -A /usr/lib/python2.7/dist-packages/bonito)" ];
 then cp -R /usr/lib/python2.7/dist-packages/bonito.init/* /usr/lib/python2.7/dist-packages/bonito
 fi
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
+exec 3>&1
+exec /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
