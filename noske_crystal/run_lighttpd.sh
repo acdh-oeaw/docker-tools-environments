15c1cc854f62d81abfabe9990c950c1985dc9794
Omar Siam <simar@gmx.net>
2021-09-24 21:55:40 +0200

    replace that strange https://localhost in crystal config automatically. Make the replacement configurable on startup so CORS can be testet.

--- noske_crystal/run_lighttpd.sh
+++ noske_crystal/run_lighttpd.sh
@@ -4,6 +4,7 @@ then cp -R /usr/lib/python2.7/site-packages/bonito.init/* /usr/lib/python2.7/dis
 fi
 if [ ! "$(ls -A /var/www/crystal)" ];
 then cp -R /var/www/crystal.init/* /var/www/crystal
+     sed -i "s~URL_BONITO: \"https://localhost/~URL_BONITO: \"${BROWSER_URL_BONITO:-}/~g" /var/www/crystal/config.js
 fi
 for corp in $CORPLIST
 do path=$(grep -h ^PATH $MANATEE_REGISTRY/$corp | sed -e 's/^PATH[^"]*"\(.*\)"$/\1/g')
