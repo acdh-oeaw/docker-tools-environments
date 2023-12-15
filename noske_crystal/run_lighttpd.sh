457246da9b5f8bdcff082fef49362b678224359e
Pirker, Hannes <hannes.pirker@oeaw.ac.at>
2023-12-15 16:03:05 +0100

    run_lighttpd.sh: allow  to be a comma-separate list; use corpinfo -p to retrieve PATH

--- noske_crystal/run_lighttpd.sh
+++ noske_crystal/run_lighttpd.sh
@@ -6,8 +6,9 @@ if [ ! "$(ls -A /var/www/crystal)" ];
 then cp -R /var/www/crystal.init/* /var/www/crystal
      sed -i "s~URL_BONITO: \"http://localhost/~URL_SKE_LI: \"about:blank\", URL_CA: \"about:blank\", URL_BONITO: \"${BROWSER_URL_BONITO:-}/~g" /var/www/crystal/config.js
 fi
-for corp in $CORPLIST
-do path=$(grep -h ^PATH $MANATEE_REGISTRY/$corp | sed -e 's/^PATH[^"]*"\(.*\)"$/\1/g')
+## corplist can be a comma-separated list!
+for corp in $(echo $CORPLIST | perl -pe 's/,/ /g'); do
+path=$(corpinfo -p $corp)
 if [ ! -d $path ] || [ ! "$(ls -A $path)" ]
   then mkdir -p $path; compilecorp $corp
   fi
